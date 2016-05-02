30df454cbac8018f47d5faf349e8c189ebd489ac
Kate Ramsey <kateramsey@live.com>
2016-05-02 16:30:55 -0500

    Initial Commit

---
+++ Lunch App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lunch_App.Startup))]
+namespace Lunch_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
