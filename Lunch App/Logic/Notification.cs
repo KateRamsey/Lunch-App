938186b6f0ec79556bd1d4e7b8e5dd9e17af6d2e
Kate Ramsey <kateramsey@live.com>
2016-05-10 17:02:22 -0500

    Added Glimpse package, Added Notification class (to send emails)

---
+++ Lunch App/Logic/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lunch_App.Logic
+{
+    public static class Notification
+    {
+        public static void InviteNotices()
+        {
+            //TODO: Hookup email notices of lunch invite
+        }
+
+        public static void LunchPlanNotices()
+        {
+            //TODO: hookup email notices of lunch plan
+        }
+    }
+}
