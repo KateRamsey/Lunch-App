abe275a3ab9d5995daa746c1d7975ca7a9adde8d
Kate Ramsey <kateramsey@live.com>
2016-05-12 11:29:18 -0500

    Comment out "Next Lunch" functionality

--- Lunch App/Logic/IndexVMLogic.cs
+++ Lunch App/Logic/IndexVMLogic.cs
@@ -43,11 +43,11 @@ namespace Lunch_App.Logic
 
             if (indexView.Lunches != null)
             {
-                indexView.Lunches = indexView.Lunches.OrderBy(x => x.MeetingDateTime).ToList();
-                foreach (var l in indexView.Lunches.Where(l => l.MeetingDateTime == DateTime.Today))
-                {
-                    indexView.NextLunch = l.MeetingDateTime;
-                }
+                //indexView.Lunches = indexView.Lunches.OrderBy(x => x.MeetingDateTime).ToList();
+                //foreach (var l in indexView.Lunches.Where(l => l.MeetingDateTime == DateTime.Today))
+                //{
+                //    indexView.NextLunch = l.MeetingDateTime;
+                //}
 
                 indexView.Lunches = indexView.Lunches.OrderByDescending(x => x.MeetingDateTime).ToList();
             }
--- Lunch App/Models/IndexVM.cs
+++ Lunch App/Models/IndexVM.cs
@@ -9,7 +9,7 @@ namespace Lunch_App.Models
     {
         public List<LunchIndexVM> Lunches { get; set; } = new List<LunchIndexVM>();
         public List<int> OutstandingSurveys { get; set; } = new List<int>();
-        public DateTime NextLunch { get; set; }
+        //public DateTime NextLunch { get; set; }
         public bool WaitingOnSurveys { get; set; }
         public List<int> LunchReadyToPick { get; set; } = new List<int>();
 
